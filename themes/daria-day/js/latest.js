e9c00cfbb60d63db0f970ffbc49aacda9e3075a9
mojopoly <cyrus.eslamian@gmail.com>
2019-03-19 11:45:44 -0400

    latest additions fully functional

--- themes/daria-day/js/latest.js
+++ themes/daria-day/js/latest.js
@@ -1,8 +1,6 @@
 ( function( $ ) {
-//console.log('hellooo from filters.js');
-console.log('hello from latest.js');
+    console.log('hello from latest.js');
 //latest addistion on front page
-    if($('#latest-additions-container').attr('data-exists')== 'yes'){
     let resultsDiv = $('#latest-additions-container');
     let image = new Array();
     let permalink = new Array();
@@ -36,26 +34,4 @@ console.log('hello from latest.js');
         </ul>`);
         
     });
-}
-//filter functionality
-$("#filter-energy-abundance").on('click', ()=> {
-    $.ajax({
-        beforeSend: (xhr) => { //this pice to pass randomly generated nonce code along with our request so wp knows we're logged in and authorized to do so
-        xhr.setRequestHeader('X-WP-Nonce', dariaData.nonce);
-        },
-        url: dariaData.root_url + '/wp-json/daria/v1/search',
-        type: 'GET', //regarding professorId below, in order to assign it dynamically, we need to assign a data attribute in html(data-professor) and then access it via js
-        // data: {'professorId': currentLikeBox.data('professor')}, //we use data property to send along info to server side and we could add it to the end of url above like ...manageLike?professorId=789, but this way is cleaner
-        success: (response)=>{
-            console.log('success');
-            console.log(response);
-        },
-        error: (response)=>{
-            console.log('try again');
-            console.log(response);
-        },
-    });
-});
-
 } )( jQuery );
-
--- themes/daria-day/template-parts/content-latest.php
+++ themes/daria-day/template-parts/content-latest.php
@@ -1,1 +1,8 @@
+
+<?php
+$existStatus = 'no';
+if (is_front_page()) {
+    $existStatus = 'yes';
+}
+?>
 <div class="latest-additions-container" id ="latest-additions-container"data-exists="<?php echo $existStatus; ?>"> Latest Additions</div>
