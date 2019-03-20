23a730bfb4612571b68149bda862cfee0198d3fb
mojopoly <cyrus.eslamian@gmail.com>
2019-03-20 09:44:21 -0400

    products filter half done

--- themes/daria-day/js/filters.js
+++ themes/daria-day/js/filters.js
@@ -16,6 +16,7 @@ console.log('hello from latest.js');
             return new Date(b.date) - new Date(a.date);
         });
         console.log(results);
+        console.log(results[0].title);
         
         for(let i = 0; i < 4 ; i++){
             latestAdditions[i].push(results[i].image);
@@ -38,17 +39,192 @@ console.log('hello from latest.js');
     });
 }
 //filter functionality
-$("#filter-btn").on('click', ()=> {
+
+$("#filter-btn").on('click', function() {
+    // $(".filter-dropdown dt").toggleClass("dt--active");
+    $(".filter-dropdown dd ul").slideToggle('fast');
+  });
+
+// $(".filter-dropdown dt a").on('click', function() {
+//     $(".filter-dropdown dd ul").slideToggle('fast');
+//   });
+  
+//   $(".filter-dropdown dd ul li a").on('click', function() {
+//     $(".filter-dropdown dd ul").hide();
+//   });
+  
+//   function getSelectedValue(id) {
+//     return $("#" + id).find("dt a span.value").html();
+//   }
+  
+  $(document).bind('click', function(e) {
+    var $clicked = $(e.target);
+    if (!$clicked.parents().hasClass("filter-dropdown")) $(".filter-dropdown dd ul").hide();
+  });
+  
+//   $('.multiselect input[type="checkbox"]').on('click', function() {
+  
+//     var title = $(this).closest('.multiselect').find('input[type="checkbox"]').val(),
+//       title = $(this).val() + ",";
+  
+//     if ($(this).is(':checked')) {
+//       var html = '<span title="' + title + '">' + title + '</span>';
+//       $('.multiSel').append(html);
+//       $(".hida").hide();
+//     } else {
+//       $('span[title="' + title + '"]').remove();
+//       var ret = $(".hida");
+//       $('.filter-dropdown dt a').append(ret);
+  
+//     }
+//   });
+//remove seat from list
+function removeSeat(seatListElm, seatValue) {
+    var arr=seatListElm.value.split(',');
+     
+     var p=arr.indexOf(seatValue);
+     if(p!=-1){
+         arr.splice(p, 1);
+         seatListElm.value=arr.join(',');
+     }
+}
+
+
+//add seat to list
+function addSeat(seatListElm, seatValue) {
+    var arr=seatListElm.value.split(',');
+    if(arr.join()==''){ arr=[]; }
+    
+    var p=arr.indexOf(seatValue);
+    if(p==-1){
+        arr.push(seatValue); //append
+        arr=arr.sort(); //sort list
+        seatListElm.value=arr.join(',');
+    } 
+}
+
+//called everytime a seat is clicked
+function seatClick(seat) {
+    seat = (this instanceof HTMLInputElement ) ? this : seat;
+    var firstSelected;
+    var selectedSeats = [];
+    var thisInputHasAlreadyBeenSeen = false;
+    var confirmedSeats = [];
+    if (seat.classList.contains('reserved')==false) {
+
+        if (seat.classList.toggle('selected')) {
+            addSeat(document.getElementById('seats'), seat.value);
+            $(".seat").each(function() {
+                if(this != seat) {
+                if(firstSelected == null && this.classList.contains('selected')) {
+                    firstSelected = this;
+                    selectedSeats.push(firstSelected);
+                    confirmedSeats = selectedSeats.slice();
+                } else if (firstSelected) {
+                    if(this.classList.contains('selected')) {
+                        selectedSeats.push(this);
+                    confirmedSeats = selectedSeats.slice();
+                       }
+                    if(!this.classList.contains('reserved')) {
+                    selectedSeats.push(this);
+                     }
+                else{
+                    if(!thisInputHasAlreadyBeenSeen) {
+                    selectedSeats = [];
+                    firstSelected = null;
+                    } else {
+                        return false;
+                    }
+                }
+                }
+                } else {
+                    selectedSeats.push(this);
+                    confirmedSeats = selectedSeats.slice();
+                    if(firstSelected == null) {
+                        thisInputHasAlreadyBeenSeen = true;
+                        firstSelected = this;
+                    }
+                }
+            });
+            if(confirmedSeats.length > 1) {
+            selectAll(confirmedSeats);
+            }
+        } else {
+            removeSeat(document.getElementById('seats'), seat.value);
+        }
+      
+    } else {
+        alert("This seat is reserved!\nPlease select another seat");
+        removeSeat(document.getElementById('seats'), seat.value);
+        return;
+    }
+}
+
+
+//adding event click to seats
+var elms=document.getElementsByClassName('seat');
+for(var i=0, l=elms.length ; i<l ; i++){
+    elms[i].onclick=seatClick;
+}
+
+function selectAll(seats) {
+    seats.forEach(function(seat) {
+        seat.className = seat.className + ' selected';
+    });
+}
+//filter process
+$("#filter-btn-interior").on('click', ()=> {
     $.ajax({
         beforeSend: (xhr) => { //this pice to pass randomly generated nonce code along with our request so wp knows we're logged in and authorized to do so
         xhr.setRequestHeader('X-WP-Nonce', dariaData.nonce);
         },
         url: dariaData.root_url + '/wp-json/daria/v1/search',
-        type: 'GET', //regarding professorId below, in order to assign it dynamically, we need to assign a data attribute in html(data-professor) and then access it via js
+        type: 'GET', 
         // data: {'professorId': currentLikeBox.data('professor')}, //we use data property to send along info to server side and we could add it to the end of url above like ...manageLike?professorId=789, but this way is cleaner
         success: (response)=>{
-            console.log('success');
-            console.log(response);
+            // console.log(response);
+            // let searchField =selectedSeats;
+            //look in tag column of each array response and return title if there are matches
+            
+            // let container = response[2].tags;
+            // let containerInc = container.includes(selectedSeats);
+            // if(selectedSeats[0].className = "seat selected") {
+            //     console.log(selectedSeats[0].defaultValue);
+            // }
+            let selSeats= selectedSeats;
+            let selSeatEmpty=[];
+            console.log(selSeats);
+            // selectedSeats.map(items => {
+            // selSeats.forEach(function(e){    
+            //     if(e.className = "seat selected"){
+            //         selSeatEmpty.push(e.defaultValue.toLowerCase());
+            //     }
+            // });
+ 
+            // response.map(item => {
+            //     //items.defaultValue.toLowerCase()
+            //         let tagContainer = item.tags;
+            //         if (selectedSeats[0].className = "seat selected" && tagContainer.includes('abundance')){
+            //             // console.log(item);
+            //             // var gridRes = response.tags;
+            //             console.log(item.title);
+            //             // $(".rowDip").append(gridRes);
+            //         }
+            // })
+
+            // if (response[0].tags.includes['silver']){
+                
+            // }
+            // $(".filter-results-container").html(`
+            // <ul class ='Filter Results'>
+            //     ${latestAdditions.map(item =>
+            //         `<li class="latest-additions-item">
+            //             <img src = ${item[0]} width="150px" height="150px">
+            //             <a href="#">${item[2]}</a>
+            //             $${item[3]} CAD
+            //         </li>`
+            //     ).join('')}
+            // </ul>`);            
         },
         error: (response)=>{
             console.log('try again');
@@ -58,4 +234,7 @@ $("#filter-btn").on('click', ()=> {
 });
 
 } )( jQuery );
+
+
+
 
--- themes/daria-day/page-product.php
+++ themes/daria-day/page-product.php
@@ -17,13 +17,66 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-            <button id="filter-btn">Filter</button>
-            <div class="filter-overlay filter-overlay--active">
+
+            <!-- <div class="filter-overlay filter-overlay--active">
                 <div class="filter-container">
-                    <strong>Filters</strong>
+                    <h1>Filters</h1>
+                    <ul class="filter-energy">
+                        <li><h2>By Energy</h2></li>
+                        <li>Abundance</li>
+                        <li>Calmness</li>
+                        <li>Enlightenment</li>
+                        <li>Fertility</li>
+                        <li>Focus</li>
+                        <li>Healing</li>
+                        <li>Love</li>
+                        <li>Positivity</li>
+                        <li>Prosperity</li>
+                    </ul>
+                    <button id="filter-btn">Apply filter</button>
                 </div>
                 <i class="fa fa-window-close filter-overlay__close" aria-hidden="true"></i>
-            </div>
+            </div> -->
+            <dl class="filter-dropdown"> 
+  
+                <!-- <dt class="dt">
+                    <a href="#">
+                        <span class="hida">Select</span>    
+                        <p class="multiSel"></p>  
+                    </a>
+                </dt> -->
+            
+                <dd>
+                    <div class="multiselect">
+                    <input id="seats" value="" class="selectedVals">
+                        <ul>
+                            <!-- <h1>By Energy</h1> -->
+
+                            <li>
+                            
+
+                            <button class="filter-energy-tag">By Energy</button>
+                            <input type="button" class="seat" value="Abundance">
+                            <input type="button" class="seat" value="Calmness">
+                            <input type="button" class="seat" value="Enlightenment">
+                            <input type="button" class="seat" value="Fertility">
+                            <input type="button" class="seat" value="Focus">
+                            <input type="button" class="seat" value="Healing">
+                            <input type="button" class="seat" value="Love">
+                            <input type="button" class="seat" value="Positivity">
+                            <input type="button" class="seat" value="Prosperity">
+                            <!-- <input type="checkbox" value="abundance" />Abundance</li>
+                            <li> -->
+                            <button class="filter-btn-interior" id="filter-btn-interior">Apply filter</button>
+
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </dd>
+            <button class="filter-btn" id="filter-btn">Filter</button>
+            </dl>
+            <div class="filter-results-container"></div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
