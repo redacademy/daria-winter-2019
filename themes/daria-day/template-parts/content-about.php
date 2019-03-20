45fc6232c63b458f2204fce6a435fb4ef8f343a4
jimmiekudo <jeffalanjohnson22@gmail.com>
2019-03-20 09:50:13 -0400

    front page slider footer and about us

--- themes/daria-day/template-parts/content-about.php
+++ themes/daria-day/template-parts/content-about.php
@@ -11,17 +11,8 @@
     <!DOCTYPE html>
     <section class = 'ourstory'>
        
+    <p><?php the_field('ourstory'); ?></p>
             
-            <div class= 'copy'>
-                <h2>Everyone has their own story. Here’s ours.</h2>
-            
-                <h4>Handcrafted Power</h4>
-                <p>At Daria Day, we use only natural, locally sourced gemstones and silver from the K2 mountains of Northern Pakistan. Through the energy of the beautiful gemstones, our jewelry can help align and transform your spirit and physical health.</p>
-
-                <h4>Life-giving Talent</h4>
-                <p>We are deeply committed to elevating the lives of the artisans by providing dignified wages and investing part of the profits in community to deliver holistic social programs that provide a path out of poverty.</p>
-            </div>
-        
             <div class='skewedphotos'>
                 <div class="photo1">
                     <h4>image placeholder</h4>
--- themes/daria-day/template-parts/content-slider.php
+++ themes/daria-day/template-parts/content-slider.php
@@ -1,47 +1,40 @@
 
 
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   
-    <link rel="stylesheet" href="main.css" type="text/css">
-</head>
-<body>
 
 <h2 class= 'title'>There are many paths, we have different options. </h2>
-    <section class='slider'>
-    
+<section class='slider'>
+
   <ul class="imgList">
 
+
+    <li id= 'bracelets'><a href='#'><img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></a>
+    <p>Bold Bracelets</p><a class='link' href = '#'><p>See more</p></a>
+    </li>
+
+
+    <li id= 'earrings'><a href='#'><img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></a><p>Energizing Earrings</p><a class='link' href='#'><p>See more</p></a>
+    </li>
+
+
+
+    <li id= 'necklaces'><a href='#'><img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></a><p>Nobel Necklaces</p><a class='link' href='#'><p>See more</p></a>
+    </li>
+
+
+    <li id= 'energy'><a href='#'><img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></a><p>Emanating Energy</p><a class='link' href='#'><p>See more</p></a></li>
     
-    <li class= 'bracelets'><a href='#'><img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></a><p>Bold Bracelets</p><a class='link' href = '#'><p>See more</p></a></li>
-  
-    
-    <li class= 'earrings'><a href='#'><img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></a><p>Energizing Earrings</p><a class='link' href='#'><p>See more</p></a></li>
-    
-    
-    
-    <li class= 'necklaces'><a href='#'><img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></a><p>Nobel Necklaces</p><a class='link' href='#'><p>See more</p></a></li>
-  
-    
-    <li class= 'energy'><a href='#'><img src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/></a><p>Emanating Energy</p><a class='link' href='#'><p>See more</p></a></li>
-    </div>
 
   </ul>
-  
-   
+
+  <div class="container">
+    <a href='#bracelets'>	<div class="btn btn1"></div></a>
+    <a href='#earrings'>	<div class="btn btn2"></div></a>
+    <a href='#necklaces'>	<div class="btn btn3"></div></a>
+    <a href='#energy'>	<div class="btn btn4"></div></a>
   </div>
-  </section>
-  
-         
-</body>
-</html>
 
 </section>
+  
+         
 
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-  </body>
-</html>
